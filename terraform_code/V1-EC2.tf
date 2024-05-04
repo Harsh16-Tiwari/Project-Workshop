72a3ada483ce70948a66e3305aff03b14500b2c2
Harsh16-Tiwari <htiwari1614@gmail.com>
2024-05-04 15:31:46 +0530

    Update rsource block

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-07caf09b362be10b8"
     instance_type = "t2.micro"
-    key_name = "dpp"
+    key_name = "Harsh"
 }
